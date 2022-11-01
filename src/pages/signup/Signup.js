6f23969097b2cb5128a9d028fd9138c53db35c4e
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-01 15:45:49 +0530

    usesignupp hook was integrated with the Signup form and image upload feature was completed.

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSignup } from "./../../hooks/useSignup";
 
 import "./Signup.css";
 
@@ -9,9 +10,11 @@ export default function Signup() {
   const [thumbnail, setThumbnail] = useState(null);
   const [thumbnailError, setThumbnailError] = useState(null);
 
+  const { signup, error, isPending } = useSignup();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName, thumbnail);
+    signup(email, password, displayName, thumbnail);
   };
 
   const handleFileChange = (e) => {
@@ -74,7 +77,13 @@ export default function Signup() {
         <input required type="file" onChange={handleFileChange} />
         {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
-      <button className="btn">Sign Up</button>
+      {!isPending && <button className="btn">Sign Up</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading...
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 }
