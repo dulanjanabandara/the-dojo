dc8155bf778c6f121f87c49d23c7e835055a1b41
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-01 22:35:09 +0530

    User avatar component was created.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,17 +1,21 @@
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "./../../hooks/useAuthContext";
 
 import DashboardIcon from "../../assets/dashboard_icon.svg";
 import AddIcon from "../../assets/add_icon.svg";
+import Avatar from "../avatar/Avatar";
 
 import "./Sidebar.css";
 
 export default function Sidebar() {
+  const { user } = useAuthContext();
+
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-          {/* Avatar and username here later */}
-          <p>Hey user</p>
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className="links">
           <ul>
