053c723b71d419e1f52a61ae73bdf44045aea7a3
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-01 18:36:50 +0530

    Redirects and route guards were added. Navbar was modified to conditionally show the links.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "./../../hooks/useAuthContext";
 import { useLogout } from "../../hooks/useLogout";
 import Temple from "../../assets/temple.svg";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const { logout, error, isPending } = useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className="navbar">
@@ -13,24 +15,32 @@ export default function Navbar() {
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Logging out...
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              {!isPending && (
+                <button className="btn" onClick={logout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" disabled>
+                  Logging out...
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
