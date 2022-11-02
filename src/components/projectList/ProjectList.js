9897258962fb53cd68edbe6de9d61e834c16b66d
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-02 19:04:49 +0530

    Refactored the project list component and styled the fetched projects.

--- src/components/projectList/ProjectList.js
+++ src/components/projectList/ProjectList.js
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
+import Avatar from "./../avatar/Avatar";
+
 import "./ProjectList.css";
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p className="error">No projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link key={project.id} to={`/projects/${project.id}`}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
 }
+
+// project.dueDate is a firestore timestamp. We want to turn it into a JS Date and then Date string. So we can output it in the browser.
