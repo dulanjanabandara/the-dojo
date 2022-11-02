471cf7c4671f518e57cb0fc0b0db7fa4048dac95
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-02 14:04:09 +0530

    Handled possible errors may occur in project category and assigned users fields.

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import Select from "react-select";
+
 import { useCollection } from "./../../hooks/useCollection";
+import Select from "react-select";
 
 import "./Create.css";
 
@@ -10,6 +11,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState("");
   const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
 
   const { documents } = useCollection("users");
   const [users, setUsers] = useState([]);
@@ -32,6 +34,18 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError(null);
+
+    if (!category) {
+      setFormError("Please select a project category.");
+      return;
+    }
+
+    if (assignedUsers.length < 1) {
+      setFormError("Please assign the project at least one user.");
+      return;
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers);
   };
 
@@ -82,6 +96,7 @@ export default function Create() {
           />
         </label>
         <button className="btn">Add project</button>
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
