3263772bc7f9f219c8fd8d2580c2125bb1fa3992
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-02 18:31:37 +0530

    Fetched projects in the dashboard.

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,16 @@
+import ProjectList from "../../components/projectList/ProjectList";
+import { useCollection } from "./../../hooks/useCollection";
+
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div></div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
