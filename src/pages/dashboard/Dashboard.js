84e62f3a28858973230ee2659ca8ca985d4bce66
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-03 20:02:55 +0530

    Functionality to filter the projects according to the category was added.

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useAuthContext } from "./../../hooks/useAuthContext";
 import ProjectList from "../../components/projectList/ProjectList";
 import ProjectFilter from "./projectFilter/ProjectFilter";
 import { useCollection } from "./../../hooks/useCollection";
@@ -9,10 +9,36 @@ import "./Dashboard.css";
 export default function Dashboard() {
   const [currentFilter, setCurrentFilter] = useState("all");
   const { documents, error } = useCollection("projects");
+  const { user } = useAuthContext();
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "marketing":
+          case "sales":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -24,7 +50,7 @@ export default function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
